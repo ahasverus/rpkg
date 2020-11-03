8539b0b5547a501c45b2f78474acdd2203da9149
Nicolas Casajus <nicolas.casajus@gmail.com>
2020-11-03 10:04:04 +0100

    :tada: First commit

---
+++ _devhistory.R
@@ -0,0 +1,8 @@
+## Development History
+
+usethis::use_build_ignore("_devhistory.R")
+
+usethis::use_build_ignore(".DS_Store")
+usethis::use_git_ignore(".DS_Store")
+
+usethis::use_git(message = ":tada: First commit")
